b82617121a0f6701ac024deecc7fcf5bf8857da2
AmberLynnR33 <103320792+AmberLynnR33@users.noreply.github.com>
2023-04-28 21:58:16 -0400

    Privatize attributes and methods
    For right now, only the initializer and the Tk object will need to be accessed outside the class. Everything else is now private.

--- View.py
+++ View.py
@@ -7,8 +7,8 @@ class View:
     The UI for the Financial Tracker
     """
     base_screen: tk.Tk
-    main_frame: tk.Frame
-    main_menu: tk.Menu
+    _main_frame: tk.Frame
+    _main_menu: tk.Menu
 
     _tab_out_frame: tk.Frame
     _money_out_frame: tk.Frame
@@ -16,44 +16,46 @@ class View:
     _display_money_frame: tk.Frame
     _goals_frame: tk.Frame
 
-
     def __init__(self):
         self.base_screen = tk.Tk()
         self._configure_main_screen
 
-        self.create_main_frame()
-        self.create_menu()
+        self._create_main_frame()
+        self._create_menu()
 
-    def create_main_frame(self) -> tk.Frame:
-        self.main_frame = tk.Frame(self.base_screen)
+    def _create_main_frame(self) -> tk.Frame:
+        self._main_frame = tk.Frame(self.base_screen)
 
         # Each subframe
-        self.frame_tab_month()
-        self.frame_money_in()
-        self.frame_money_out()
-        self.frame_display_money()
-        self.frame_goals()
+        self._frame_tab_month()
+        self._frame_money_in()
+        self._frame_money_out()
+        self._frame_display_money()
+        self._frame_goals()
 
-    def frame_tab_month(self) -> None:
-        self._tab_month_frame = tk.Frame(self.main_frame)
+    def _frame_tab_month(self) -> None:
+        self._tab_month_frame = tk.Frame(self._main_frame)
 
-    def frame_money_out(self) -> None:
-        self._money_out_frame = tk.Frame(self.main_frame)
+    def _frame_money_out(self) -> None:
+        self._money_out_frame = tk.Frame(self._main_frame)
 
-    def frame_money_in(self) -> None:
-        self._money_in_frame = tk.Frame(self.main_frame)
+    def _frame_money_in(self) -> None:
+        self._money_in_frame = tk.Frame(self._main_frame)
 
-    def frame_display_money(self) -> None:
-        self._display_money_frame = tk.Frame(self.main_frame)
+    def _frame_display_money(self) -> None:
+        self._display_money_frame = tk.Frame(self._main_frame)
 
-    def frame_goals(self) -> None:
-        self._goals_frame = tk.Frame(self.main_frame)
+    def _frame_goals(self) -> None:
+        self._goals_frame = tk.Frame(self._main_frame)
 
-    def create_menu(self) -> None:
-        pass
+    def _create_menu(self) -> None:
+        self._main_menu = tk.Menu(self.base_screen)
+
 
     def _configure_main_screen(self) -> None:
-        self.main_frame.grid(row=0, col=0, sticky='n s e w')
-        self.main_frame.columnconfigure(0, weight=1)
-        self.main_frame.rowconfigure(0, weight=1)
+        self._main_frame.grid(row=0, col=0, sticky='n s e w')
+        self._main_frame.columnconfigure(0, weight=1)
+        self._main_frame.rowconfigure(0, weight=1)
         self.base_screen.title("Financial Tracker")
+
+        self.base_screen['menu'] = self._main_menu
