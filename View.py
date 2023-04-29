00cdb5acddc0bfa64e3dffe039fc4d49b97b5f92
AmberLynnR33 <103320792+AmberLynnR33@users.noreply.github.com>
2023-04-28 21:52:46 -0400

    Make each frame
    - Each frame initialized in their respective methods
    - Each frame is called in the creation of the main frame
    - Types added to the top of the file

--- View.py
+++ View.py
@@ -20,20 +20,27 @@ class View:
     def create_main_frame(self) -> tk.Frame:
         self.main_frame = tk.Frame(self.base_screen)
 
+        # Each subframe
+        self.frame_tab_month()
+        self.frame_money_in()
+        self.frame_money_out()
+        self.frame_display_money()
+        self.frame_goals()
+
     def frame_tab_month(self) -> None:
-        pass
+        tab_month_frame = tk.Frame(self.main_frame)
 
     def frame_money_out(self) -> None:
-        pass
+        money_out_frame = tk.Frame(self.main_frame)
 
     def frame_money_in(self) -> None:
-        pass
+        money_in_frame = tk.Frame(self.main_frame)
 
     def frame_display_money(self) -> None:
-        pass
+        display_money_frame = tk.Frame(self.main_frame)
 
     def frame_goals(self) -> None:
-        pass
+        goals_frame = tk.Frame(self.main_frame)
 
     def create_menu(self) -> None:
         pass
