8e7fba6f0ee332f8897c786434c5bba75d530696
AmberLynnR33 <103320792+AmberLynnR33@users.noreply.github.com>
2023-04-28 21:44:01 -0400

    Change type contracts of methods

--- View.py
+++ View.py
@@ -14,28 +14,29 @@ class View:
         self.base_screen = tk.Tk()
         self.base_screen.title("Financial Tracker")
 
-        self.main_frame = self.create_main_frame()
+        self.create_main_frame()
+        self.create_menu()
+
+    def create_main_frame(self) -> tk.Frame:
+        self.main_frame = tk.Frame(self.base_screen)
+
+    def frame_tab_month(self) -> None:
+        pass
+
+    def frame_money_out(self) -> None:
+        pass
+
+    def frame_money_in(self) -> None:
+        pass
+
+    def frame_display_money(self) -> None:
+        pass
+
+    def frame_goals(self) -> None:
+        pass
+
+    def create_menu(self) -> None:
+        pass
+
+    def _configure_main_screen(self) -> None:
         self.main_frame.grid(row=0, col=0, sticky='n s e w')
-
-        self.main_menu = self.create_menu()
-
-    def create_main_frame() -> tk.Frame:
-        pass
-
-    def frame_tab_month() -> tk.Frame:
-        pass
-
-    def frame_money_out() -> tk.Frame:
-        pass
-
-    def frame_money_in() -> tk.Frame:
-        pass
-
-    def frame_display_money() -> tk.Frame:
-        pass
-
-    def frame_goals() -> tk.Frame:
-        pass
-
-    def create_menu() -> tk.Menu:
-        pass
